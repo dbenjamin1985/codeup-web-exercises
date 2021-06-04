81d4201e5a028e961f5bb23b8e7a6648a3f9768d
David Benjamin <david.j.benjamin67@gmail.com>
2021-06-04 13:27:00 -0500

    for loop exercises

---
+++ js/for_loops.js
@@ -0,0 +1,23 @@
+// # 2
+function showMultiplicationTable (x) {
+    for (var i = 1; i <= 10; i++) {
+        console.log(x + " x " + i + " = " + (x * i));
+    }
+}
+showMultiplicationTable(7);
+
+// # 3
+for (let ev = 0; ev<10; ev++) {
+    let random = Math.ceil(Math.random() * 200) + 20;
+    if (random % 2 !== 0) {
+        console.log(random + " is odd");
+    } else {
+        console.log(random + " is even");
+    }
+}
+
+// # 4
+for ()
+
+
+
---
+++ js/loop_classwork.js
@@ -0,0 +1,6 @@
+function showMultiplicationTable (10) {
+    for (var i = 1; i < 10; i++) {
+        console.log('for loop iteration #' + i);
+    }
+}
+
