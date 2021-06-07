16d37876434e74d834a52b5d803a4f548c54f7ac
David Benjamin <david.j.benjamin67@gmail.com>
2021-06-07 13:24:29 -0700

    Planets Split/Join arrays exercises

--- js/planets-string.js
+++ js/planets-string.js
@@ -23,5 +23,6 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
-
+    var planetsString2 = "Mercury \ Venus \ Earth \ Mars \ Jupiter \ Saturn \ Uranus \ Neptune";
+    console.log(planetsString2);
 })();
