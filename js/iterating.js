6c37cd84a556414befae67ed94d0b006334e7e90
David Benjamin <david.j.benjamin67@gmail.com>
2021-06-07 12:43:19 -0700

    Arrays exercises

--- js/iterating.js
+++ js/iterating.js
@@ -45,3 +45,21 @@
      */
 
 })();
+
+var names = ['Sam', 'Steve', 'Bucky', 'Sharon'];
+
+console.log(names.length);
+
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+
+for(var i = 0; i < names.length; i++) {
+    console.log('The name is ' + names[i]);
+}
+
+names.forEach(function(name) {
+    console.log('The name is: ' + name + '.');
+});
+
